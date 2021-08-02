8c0c62152ca7b071c17df2716420e077b8b07e74
sapovadia <hn525278@dal.ca>
2021-08-02 15:09:34 +0530

    FIxes in UploadRecipe

--- src/pages/UploadRecipe.js
+++ src/pages/UploadRecipe.js
@@ -9,7 +9,6 @@ import axios from 'axios';
 
 const UploadRecipe = () => {
   const [uploadFile, setUploadFile] = useState();
-  const [superHero, setSuperHero] = useState();
   const [selectedFile, setSelectedFile] = useState(null);
   const [text, setText] = useState('');
   const [response, setResponse] = useState('');
@@ -17,15 +16,7 @@ const UploadRecipe = () => {
   // On file upload (click the upload button)
   const onFileUpload = (event) => {
     event.preventDefault();
-    // Create an object of formData
-    // const formData = new FormData();
-
-    // Update the formData object
-    // formData.append('myRecipe', selectedFile, selectedFile.name);
-
-    // Details of the uploaded file
-    // console.log(selectedFile);
-    // console.log(formData);
+    
     console.log(text);
     axios({
       method: 'post',
@@ -55,51 +46,6 @@ const UploadRecipe = () => {
     // selectedFile: event.target.files[0];
   };
 
-  const submitForm = (event) => {
-    event.preventDefault();
-
-    const dataArray = new FormData();
-    dataArray.append('superHeroName', superHero);
-    dataArray.append('uploadFile', uploadFile);
-
-    axios
-      .post('url here', dataArray, {
-        // paste url here!
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      })
-      .then((response) => {
-        alert(response);
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  };
-  // File content to be displayed after
-  // file upload is complete
-  const fileData = () => {
-    if (selectedFile) {
-      return (
-        <div>
-          <h2>File Details:</h2>
-
-          <p>File Name: {selectedFile.name}</p>
-
-          <p>File Type: {selectedFile.type}</p>
-
-          {/* <p>Last Modified: {selectedFile.lastModifiedDate.toDateString()}</p> */}
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <br />
-          <h4>Choose before Pressing the Upload button</h4>
-        </div>
-      );
-    }
-  };
   const handleTextBox = (event) => {
     setText(event.target.value);
     // console.log(event.target.value);
@@ -116,10 +62,9 @@ const UploadRecipe = () => {
               onSubmit={onFileUpload}
             >
               <input type='file' onChange={onFileChange} />
-              <button onClick={onFileUpload}>Upload!</button>
+              <button onClick={onFileUpload}>Predict!</button>
             </form>
           </div>
-          {/* {fileData()} */}
           <textarea onChange={handleTextBox} value={text}></textarea>
         </div>
       </div>
@@ -128,20 +73,3 @@ const UploadRecipe = () => {
 };
 
 export default UploadRecipe;
-
-/* <iframe
-            width='600'
-            height='450'
-            src='https://datastudio.google.com/embed/reporting/feb6ed1e-88ce-4626-bc02-bf80562c3328/page/DzeWC'
-            frameborder='0'
-            // style='border:0'
-            allowfullscreen
-          ></iframe>
-          <iframe
-            width='600'
-            height='450'
-            src='https://datastudio.google.com/embed/reporting/364a5d52-4acb-4b78-b3d7-437bb7606a4a/page/c9eWC'
-            frameborder='0'
-            // style='border:0'
-            allowfullscreen
-          ></iframe> */
